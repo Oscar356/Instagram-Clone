6def32083579f4cbfabedfd6b9777d90184b837e
Oscar <68169051+Oscar356@users.noreply.github.com>
2021-01-30 14:44:15 -0800

    Feed and FeedPost updates

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -1,10 +1,92 @@
 import React from "react";
 import { useFeedPostStyles } from "../../styles";
+import UserCard from "../shared/UserCard";
+import { CommentIcon, MoreIcon, ShareIcon } from "../../icons";
+import { Link } from "react-router-dom";
+import { Button, Typography } from "@material-ui/core";
+import HTMLEllipsis from "react-lines-ellipsis/lib/html";
 
-function FeedPost() {
-  useFeedPostStyles();
+function FeedPost({ post }) {
+  const classes = useFeedPostStyles();
+  const [showCaption, setCaption] = React.useState(false);
+  const { id, media, likes, user, caption, comments } = post;
 
-  return <div>FeedPost</div>;
+  return (
+    <>
+      <article className={classes.article}>
+        {/* Feed Post Header */}
+        <div className={classes.postHeader}>
+          <UserCard />
+          <MoreIcon className={classes.moreIcon} />
+        </div>
+        {/* Feed Post Image */}
+        <div>
+          <img src={media} alt="Post media" className={classes.image} />
+        </div>
+        {/* Feed Post Buttons */}
+        <div className={classes.postButtonsWrapper}>
+          <div className={classes.postButtons}>
+            {/* <LikeButton /> */}
+            <Link to={`/p/${id}`}>
+              <CommentIcon />
+            </Link>
+            <ShareIcon />
+            {/* <SaveButton /> */}
+          </div>
+          <Typography className={classes.like} variant="subtitle2">
+            <span>{likes === 1 ? "1 like" : `${likes} likes`}</span>
+          </Typography>
+          <div className={showCaption ? classes.expanded : classes.collapsed}>
+            <Link to={`/${user.username}`}>
+              <Typography
+                variant="subtitle2"
+                component="span"
+                className={classes.username}
+              >
+                {user.username}
+              </Typography>
+            </Link>
+            {/* This section controls if the captions displayed are expanded or not */}
+            {showCaption ? (
+              <Typography
+                variant="body2"
+                component="span"
+                dangerouslySetInnerHTML={{ __html: caption }}
+              />
+            ) : (
+              <div className={classes.captionWrapper}>
+                <HTMLEllipsis
+                  unsafeHTML={caption}
+                  className={classes.caption}
+                  maxLine="0"
+                  ellipsis="..."
+                  basedOn="letters"
+                />
+                <Button
+                  className={classes.moreButton}
+                  onClick={() => setCaption(true)}
+                >
+                  more
+                </Button>
+              </div>
+            )}
+          </div>
+          <Link to={`/p/${id}`}>
+            <Typography 
+            className={classes.commentsLink}
+            variant="body2"
+            component="div">
+              View all {comments.length} comments
+            </Typography>
+          </Link>
+        </div>
+      </article>
+    </>
+  );
 }
+
+// function LikeButton() {}
+
+// function SaveButton() {}
 
 export default FeedPost;
