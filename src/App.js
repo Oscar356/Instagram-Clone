54fdf5c56e2c6fca3e30f3c9f30ffab6c20e25c8
Oscar <68169051+Oscar356@users.noreply.github.com>
2021-02-08 17:00:01 -0800

    Explore and Post page Updates

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
-import FeedPage from './pages/feed';
-import ExplorePage from './pages/explore';
-import ProfilePage from './pages/profile';
-import PostPage from './pages/post';
-import EditProfilePage from './pages/edit-profile';
-import LoginPage from './pages/login';
-import SignUpPage from './pages/signup';
-import NotFoundPage from './pages/not-found';
-
+import { Switch, Route, useHistory, useLocation } from "react-router-dom";
+import FeedPage from "./pages/feed";
+import ExplorePage from "./pages/explore";
+import ProfilePage from "./pages/profile";
+import PostPage from "./pages/post";
+import EditProfilePage from "./pages/edit-profile";
+import LoginPage from "./pages/login";
+import SignUpPage from "./pages/signup";
+import NotFoundPage from "./pages/not-found";
+import PostModal from "./components/post/PostModal";
 
 function App() {
+  const history = useHistory();
+  const location = useLocation();
+  const prevLocation = React.useRef(location);
+  const modal = location.state?.modal;
+  // This code makes it so when you first click on a post in the discover page,
+  // that the post will show with the discover page in the background but if you refresh
+  // the page that it will take you to the post page.
+  React.useEffect(() => {
+    if (history.action !== "POP" && !modal) {
+      prevLocation.current = location;
+    }
+  }, [location, modal, history.action]);
+
+  const isModalOpen = modal && prevLocation.current !== location;
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={FeedPage}/>
-        <Route path="/explore" component={ExplorePage}/>
-        <Route exact path="/:username" component={ProfilePage}/>
-        <Route exact path="/p/:postId" component={PostPage}/>
-        <Route path="/accounts/edit" component={EditProfilePage}/>
-        <Route path="/accounts/login" component={LoginPage}/>
-        <Route path="/accounts/emailsignup" component={SignUpPage}/>
-        <Route path="*" component={NotFoundPage}/>
+    <>
+      <Switch location={isModalOpen ? prevLocation.current : location}>
+        <Route exact path="/" component={FeedPage} />
+        <Route path="/explore" component={ExplorePage} />
+        <Route exact path="/:username" component={ProfilePage} />
+        <Route exact path="/p/:postId" component={PostPage} />
+        <Route path="/accounts/edit" component={EditProfilePage} />
+        <Route path="/accounts/login" component={LoginPage} />
+        <Route path="/accounts/emailsignup" component={SignUpPage} />
+        <Route path="*" component={NotFoundPage} />
       </Switch>
-    </Router>
-  )
+      {isModalOpen && <Route exact path="/p/:postId" component={PostModal} />}
+    </>
+  );
 }
 
 export default App;
