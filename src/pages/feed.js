d781db9fe04d5d2307e2e16f95613f897249b663
Oscar <68169051+Oscar356@users.noreply.github.com>
2021-02-10 16:10:05 -0800

    Profile page Updates

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -2,12 +2,14 @@ import React from "react";
 import { useFeedPageStyles } from "../styles";
 import Layout from "../components/shared/Layout";
 import UserCard from "../components/shared/UserCard";
-import FeedPost from "../components/feed/FeedPost";
+// import FeedPost from "../components/feed/FeedPost";
 import FeedSideSuggestions from "../components/feed/FeedSideSuggestions";
 import { getDefaultPost } from "../data";
 import { Hidden } from "@material-ui/core";
 import LoadingScreen from "../components/shared/LoadingScreen";
 import { LoadingLargeIcon } from "../icons";
+import FeedPostSkeleton from "../components/feed/FeedPostSkeleton";
+const FeedPost = React.lazy(() => import("../components/feed/FeedPost"));
 
 function FeedPage() {
   const classes = useFeedPageStyles();
@@ -17,27 +19,33 @@ function FeedPage() {
   let loading = false;
   if (loading) return <LoadingScreen />;
 
-  return <Layout>
-    <div className={classes.container}>
-      {/* Feed Posts */}
-      <div>
-        {Array.from({ length: 5 }, () => getDefaultPost()).map((post, index) => (
-          <FeedPost key={post.id} index={index} post={post} />
-        ))}
-      </div>
-      {/* Sidebar */}
-      <Hidden smDown>
+  return (
+    <Layout>
+      <div className={classes.container}>
+        {/* Feed Posts */}
+        <div>
+          {Array.from({ length: 5 }, () => getDefaultPost()).map(
+            (post, index) => (
+              <React.Suspense key={post.id} fallback={<FeedPostSkeleton />}>
+                <FeedPost  index={index} post={post} />
+              </React.Suspense>
+            )
+          )}
+        </div>
+        {/* Sidebar */}
+        <Hidden smDown>
           <div className={classes.sidebarContainer}>
             <div className={classes.sidebarWrapper}>
               <UserCard avatarSize={50} />
               <FeedSideSuggestions />
             </div>
           </div>
-      </Hidden>
-      {/* This is in charge or loading icon at the bottom of page */}
-      {!isEndOfFeed && <LoadingLargeIcon />}
-    </div>
-  </Layout>
+        </Hidden>
+        {/* This is in charge or loading icon at the bottom of page */}
+        {!isEndOfFeed && <LoadingLargeIcon />}
+      </div>
+    </Layout>
+  );
 }
 
 export default FeedPage;
